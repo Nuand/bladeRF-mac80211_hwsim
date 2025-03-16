e85858b58ec3003ddae776155e428e2e20791137
alphafox02 <44436101+alphafox02@users.noreply.github.com>
2025-03-15 21:00:13 -0400

    newer kernel API, these callbacks now include an extra parameter

--- mac80211_hwsim.c
+++ mac80211_hwsim.c
@@ -2735,21 +2735,25 @@ static void mac80211_hwsim_change_chanctx(struct ieee80211_hw *hw,
 }
 
 static int mac80211_hwsim_assign_vif_chanctx(struct ieee80211_hw *hw,
-					     struct ieee80211_vif *vif,
-					     struct ieee80211_chanctx_conf *ctx)
+                                             struct ieee80211_vif *vif,
+                                             struct ieee80211_bss_conf *info,
+                                             struct ieee80211_chanctx_conf *ctx)
 {
-	hwsim_check_magic(vif);
-	hwsim_check_chanctx_magic(ctx);
+    (void)info;  // ignore if unused
+    hwsim_check_magic(vif);
+    hwsim_check_chanctx_magic(ctx);
 
-	return 0;
+    return 0;
 }
 
 static void mac80211_hwsim_unassign_vif_chanctx(struct ieee80211_hw *hw,
-						struct ieee80211_vif *vif,
-						struct ieee80211_chanctx_conf *ctx)
+                                                struct ieee80211_vif *vif,
+                                                struct ieee80211_bss_conf *info,
+                                                struct ieee80211_chanctx_conf *ctx)
 {
-	hwsim_check_magic(vif);
-	hwsim_check_chanctx_magic(ctx);
+    (void)info;  // ignore if unused
+    hwsim_check_magic(vif);
+    hwsim_check_chanctx_magic(ctx);
 }
 
 static const char mac80211_hwsim_gstrings_stats[][ETH_GSTRING_LEN] = {
