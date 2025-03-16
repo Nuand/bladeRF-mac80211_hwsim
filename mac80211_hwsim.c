22f11f1119aafa76f458177de7910e57819a2153
alphafox02 <44436101+alphafox02@users.noreply.github.com>
2025-03-15 20:48:31 -0400

    callback prototype now expects the fourth argument to be of type u64

--- mac80211_hwsim.c
+++ mac80211_hwsim.c
@@ -2111,7 +2111,7 @@ static void mac80211_hwsim_bcn_en_iter(void *data, u8 *mac,
 static void mac80211_hwsim_bss_info_changed(struct ieee80211_hw *hw,
 					    struct ieee80211_vif *vif,
 					    struct ieee80211_bss_conf *info,
-					    u32 changed)
+					    u64 changed)
 {
 	struct hwsim_vif_priv *vp = (void *)vif->drv_priv;
 	struct mac80211_hwsim_data *data = hw->priv;
