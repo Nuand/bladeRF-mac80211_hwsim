09e53843f6d5411e649d3b1d774c78f01ea7016a
alphafox02 <44436101+alphafox02@users.noreply.github.com>
2025-03-15 20:56:32 -0400

    update adds the new link_id parameter and renames the old queue parameter to ac to match the kernel’s updated API

--- mac80211_hwsim.c
+++ mac80211_hwsim.c
@@ -2280,16 +2280,19 @@ static int mac80211_hwsim_set_tim(struct ieee80211_hw *hw,
 }
 
 static int mac80211_hwsim_conf_tx(
-	struct ieee80211_hw *hw,
-	struct ieee80211_vif *vif, u16 queue,
-	const struct ieee80211_tx_queue_params *params)
+    struct ieee80211_hw *hw,
+    struct ieee80211_vif *vif,
+    unsigned int link_id,
+    u16 ac,
+    const struct ieee80211_tx_queue_params *params)
 {
-	wiphy_dbg(hw->wiphy,
-		  "%s (queue=%d txop=%d cw_min=%d cw_max=%d aifs=%d)\n",
-		  __func__, queue,
-		  params->txop, params->cw_min,
-		  params->cw_max, params->aifs);
-	return 0;
+    (void) link_id; // Ignore if unused
+    wiphy_dbg(hw->wiphy,
+              "%s (ac=%u, txop=%d, cw_min=%d, cw_max=%d, aifs=%d)\n",
+              __func__, ac,
+              params->txop, params->cw_min,
+              params->cw_max, params->aifs);
+    return 0;
 }
 
 static int mac80211_hwsim_get_survey(struct ieee80211_hw *hw, int idx,
